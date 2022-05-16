fb04514eed75b1b5a64ce628d90ad10476e21a45
RomanKhrapai <hrapayr@gmail.com>
2022-05-16 19:17:09 +0300

    +

--- index.js
+++ index.js
@@ -37,28 +37,3 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(arg);
-
-// function invokeAction({ action, id, name, email, phone }) {
-//     switch (action) {
-//         case "list":
-//             contacts.listContacts();
-//             break;
-
-//         case "get":
-//             contacts.getContactById(id);
-//             break;
-
-//         case "add":
-//             contacts.addContact(name, email, phone);
-//             break;
-
-//         case "remove":
-//             contacts.removeContact(id);
-//             break;
-
-//         default:
-//             console.warn("\x1B[31m Unknown action type!");
-//     }
-// }
-
-// invokeAction(argv);
